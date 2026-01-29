37c0d8b7a1808dbc2cb0c29eca74e3b69b2e3598
idsinternship <ids.internship@outlook.com>
2026-01-29 15:58:52 +0000

    876545678

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -15,3 +15,16 @@ Route::get('/courses/{id}', [CourseController::class, 'show']);
 
 // LESSONS
 Route::get('/lessons/{id}', [LessonController::class, 'show']);
+
+Route::get('/quizzes/{id}', [QuizController::class, 'show']);
+Route::post('/quizzes/{id}/submit', [QuizController::class, 'submit']);
+
+
+use App\Http\Controllers\Instructor\CourseController as InstructorCourseController;
+
+Route::middleware('auth:api')->prefix('instructor')->group(function () {
+    Route::get('/courses', [InstructorCourseController::class, 'index']);
+    Route::post('/courses', [InstructorCourseController::class, 'store']);
+    Route::put('/courses/{id}', [InstructorCourseController::class, 'update']);
+    Route::delete('/courses/{id}', [InstructorCourseController::class, 'destroy']);
+});
